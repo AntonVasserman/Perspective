50220214561da474c3c6aef374db285a97a251b8
Anton Vasserman <Anton.Vasserman@outlook.com>
2025-02-24 08:46:52 +0200

    Refactor(na): Deleted RectGate

--- Source/Perspective/Public/Core/Utility/PRSStatics.h
+++ Source/Perspective/Public/Core/Utility/PRSStatics.h
@@ -13,26 +13,6 @@ class UPRSStatics : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	FORCEINLINE static UStaticMesh* GetCubeStaticMesh()
-	{
-		return LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Cube.Cube"));
-	}
-
-	FORCEINLINE static UMaterialInterface* GetPanelGreenMaterial()
-	{
-		return LoadObject<UMaterialInterface>(nullptr, TEXT("/Game/Perspective/Operatables/Gates/M_Gate_Green.M_Gate_Green"));
-	}
-
-	FORCEINLINE static UMaterialInterface* GetPanelYellowMaterial()
-	{
-		return LoadObject<UMaterialInterface>(nullptr, TEXT("/Game/Perspective/Operatables/Gates/M_Gate_Yellow.M_Gate_Yellow"));
-	}
-
-	FORCEINLINE static UMaterialInterface* GetPanelRedMaterial()
-	{
-		return LoadObject<UMaterialInterface>(nullptr, TEXT("/Game/Perspective/Operatables/Gates/M_Gate_Red.M_Gate_Red"));
-	}
-
 	FORCEINLINE static UMaterialParameterCollection* GetMaskingMaterialParameterCollection()
 	{
 		return LoadObject<UMaterialParameterCollection>(nullptr, TEXT("/Game/Perspective/Art/Shared/Materials/MPC_Masking.MPC_Masking"));
